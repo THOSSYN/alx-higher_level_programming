87a9f3dca748920dc2f1744383aad96b26a26abf
THOSSYN <alextosin5@gmail.com>
2023-06-06 13:33:28 -0700

    Fix: FizzBuzz errors

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+#12-fizzbuzz.py
+
 def fizzbuzz():
+    """Prints Fizz, Buzz for the numbers from 1 to 100
+    seperated by a space"""
     for x in range(1, 101):
         if x % 3 == 0 and x % 5 == 0 and x % 15 == 0:
             print("FizzBuzz", end = " ")
