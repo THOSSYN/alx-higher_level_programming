2f279bf6921d0e0325f81068b77e78c769794fed
THOSSYN <alextosin5@gmail.com>
2023-06-06 12:52:04 -0700

    Fix: 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,11 +2,8 @@
 #5-print_comb2.py
 
 #Prints numbers from 0 to 99
-num = 0
-
-while num <= 99:
+for num in range(0, 100):
     if num == 99:
         print("{}".format(num))
     else:
         print("{:02}".format(num), end=", ")
-    num += 1
