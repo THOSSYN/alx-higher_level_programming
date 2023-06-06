e519d4b45b2dbc54fca21be2dd73d4d40fead7f5
THOSSYN <alextosin5@gmail.com>
2023-06-06 12:47:56 -0700

    Fix: 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -5,12 +5,8 @@
 num = 0
 
 while num <= 99:
-    if num < 10:
-        print(F"0{num:d}", end=", ")
-    elif num > 10:
-        if num == 99:
-            print(num)
-            break
-        else:
-            print(F"{num:d}", end=", ")
+    if num == 99:
+        print("{}".format(num))
+    else:
+        print("{:02}".format(num), end=", ")
     num += 1
