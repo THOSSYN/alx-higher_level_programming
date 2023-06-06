f69ea1616b41b1affbb9b39391acaf8b8ccab6e4
THOSSYN <alextosin5@gmail.com>
2023-06-06 11:49:19 -0700

    Fix: bugs in 3-print_alphabet.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+#3-print_alphabt.py
+
+"""Prints the ASCII alphabets in lowercase
+not followed by a new line"""
 for chars in range(97, 123):
     if chars == 113 or chars == 101:
         continue
-    print(chr(chars), end="")
+    print("{}".format(chr(chars)), end="")
