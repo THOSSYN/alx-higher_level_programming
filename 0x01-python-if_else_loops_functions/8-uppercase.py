abd37b0a72ae375c797ee38aa1fc540478549aef
THOSSYN <alextosin5@gmail.com>
2023-06-06 13:18:24 -0700

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+#8-uppercase.py
+
 def uppercase(string):
+    """Prints a string in uppercase."""
     for x in string:
         if x >= 'a' and x <= 'z':
             x = chr(ord(x) - 32)
