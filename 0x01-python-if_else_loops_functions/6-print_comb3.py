fdaa33ea093bf6e3468b3fc3e88ccc18685d0430
THOSSYN <alextosin5@gmail.com>
2023-06-06 13:09:43 -0700

    Fix: 6-print_combo3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-outer = 0
-while outer < 8:
-    inner = 1
-    while inner <= 9:
-        print(f"{outer}{inner}", end = ", ")
-        inner += 1
-    outer += 1
+#6-print_comb3.py
+
+"""Prints all possible different combination
+of two digits."""
+for outer in range(0, 10):
+    for inner in range(outer + 1, 10):
+        if outer == 8 and inner == 9:
+            print("{}{}".format(outer, inner))
+        else:
+            print("{}{}".format(outer, inner), end=", ")
