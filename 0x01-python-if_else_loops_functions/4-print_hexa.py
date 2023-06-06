64cf60b3dac91910da4f09ab6276619a6160c396
THOSSYN <alextosin5@gmail.com>
2023-06-06 12:04:47 -0700

    Fix: some issues with code failure

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+#4-print_hexa.py
+
+"""Prints all numbers from 0 to 98 in decimal and
+in hexadecimal"""
 i = 0
+
 while i <= 98:
-    print(f"{i} = 0x{i:x}")
+    print("{:d} = {:x}".format(i, i))
     i += 1
