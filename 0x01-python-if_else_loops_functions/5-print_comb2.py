93a5db61c927957598218046227605fe8b689714
THOSSYN <alextosin5@gmail.com>
2023-06-07 05:38:11 -0700

    Fix: format spec in task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-# 5-print_comb2.py
-
-"""Prints numbers from 0 to 99.Numbers must be
-separated by , followed by a space."""
-for num in range(0, 100):
-    if num != 99:
-        print("{:02d}".format(num), end=', ')
+for i in range(0, 100):
+    if i != 99:
+        print("{:02d}".format(i), end=', ')
     else:
-        print("{:02d}".format(num))
+        print("{:02d}".format(i))
