6d5238d64f63ad563e53a75cbd2aa9518d006b09
THOSSYN <alextosin5@gmail.com>
2023-06-08 05:58:34 -0700

    Test: 2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    arg_count = len(sys.argv) - 1
+    if arg_count == 0:
+        print("{} arguments.".format(arg_count))
+    elif arg_count == 1:
+        print("{} argument".format(arg_count))
+    else:
+        print("{} arguments:".format(arg_count))
+    for args in range(arg_count):
+        print("{} : {}".format(args + 1, sys.argv[args + 1]))
