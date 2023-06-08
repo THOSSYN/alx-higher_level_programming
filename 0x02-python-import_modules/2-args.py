2c9dd67b2f235e69696c744222d6c357ae238f23
THOSSYN <alextosin5@gmail.com>
2023-06-08 06:03:33 -0700

    Test: 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
     if arg_count == 0:
         print("{} arguments.".format(arg_count))
     elif arg_count == 1:
-        print("{} argument".format(arg_count))
+        print("{} argument:".format(arg_count))
     else:
         print("{} arguments:".format(arg_count))
     for args in range(arg_count):
-        print("{} : {}".format(args + 1, sys.argv[args + 1]))
+        print("{}: {}".format(args + 1, sys.argv[args + 1]))
