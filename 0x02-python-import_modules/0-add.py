51e68c28779ae5e227f0e250bf95a6e2ebc2b696
THOSSYN <alextosin5@gmail.com>
2023-06-08 04:56:55 -0700

    Trying 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
-from add_0 import add
-a = 1
-b = 2
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    from add_0 import add
+    """0-add.py add two variable a and b by importing add-0
+    Arguments:
+        a: first parameter
+        b: second parameter
+
+    Returns:
+        The value a + b
+    """
+    a = 1
+    b = 2
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
