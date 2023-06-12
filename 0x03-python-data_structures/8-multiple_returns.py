f6084559458f96cdd67195180658630e6d7dec4b
THOSSYN <alextosin5@gmail.com>
2023-06-12 09:36:23 -0700

    Fix:correct output 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -6,9 +6,7 @@ def multiple_returns(sentence):
        Args:
         first arg: sentence
     """
-    count = 0
-    for i in range(len(sentence)):
-        if len(sentence) == 0:
-            sentence[0] = None
-        count += 1
-    return (count), (sentence[0])
+    if sentence != "":
+        return len(sentence), sentence[0]
+    elif sentence == "":
+        return len(sentence), None;
