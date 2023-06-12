a78157c3ac8bdeb172578b9734cb3965ec02632a
THOSSYN <alextosin5@gmail.com>
2023-06-12 06:26:50 -0700

    Fix: Requirement failure on 3-print_reversed_list_integer.py

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -6,6 +6,6 @@ def print_reversed_list_integer(my_list=[]):
        Args:
         first_arg: my_list=[]
     """
-    list_range = len(my_list) - 1
-    for i in range(list_range, -1, -1):
-        print("{}".format(my_list[i]))
+    my_list.reverse()
+    for i in my_list:
+        print("{:d}".format(i))
