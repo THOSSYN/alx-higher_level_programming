3f292b6e2193f0fa8f903e98dfe7c0a78d9e7b50
THOSSYN <alextosin5@gmail.com>
2023-06-12 07:18:58 -0700

    Fix: check failure 1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -11,5 +11,5 @@ def element_at(my_list, idx):
     """
     list_range = len(my_list) - 1
     if idx >= 0 and idx <= list_range:
-            return my_list[idx]
+        return my_list[idx]
     return None
