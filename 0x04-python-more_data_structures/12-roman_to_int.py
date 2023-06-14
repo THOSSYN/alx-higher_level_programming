fe004455388e477555403a686dde981d3e954612
THOSSYN <alextosin5@gmail.com>
2023-06-14 11:22:22 -0700

    Test: 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -8,25 +8,26 @@ def roman_to_int(roman_string):
 
        Return: 0 if roman_string is None
     """
-    if roman_string:
-        roman_dict = {
-                'I': 1,
-                'IV': 4,
-                'V': 5,
-                'IX': 9,
-                'X': 10,
-                'XL': 40,
-                'L': 50,
-                'C': 100,
-                'CD': 400,
-                'D': 500,
-                'CM': 900,
-                'M': 1000
-                }
-        total = 0
-        for x, y in roman_dict.items():
-            for char in roman_string:
-                if char == x:
-                    total += y
-        return total
-    return 0
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+
+    roman_dict = {
+            'I': 1,
+            'IV': 4,
+            'V': 5,
+            'IX': 9,
+            'X': 10,
+            'XL': 40,
+            'L': 50,
+            'C': 100,
+            'CD': 400,
+            'D': 500,
+            'CM': 900,
+            'M': 1000
+            }
+    total = 0
+    for x, y in roman_dict.items():
+        for char in roman_string:
+            if char == x:
+                total += y
+    return total
