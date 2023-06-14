2ec93baa4658504fac809839ce4b42c63424ed95
THOSSYN <alextosin5@gmail.com>
2023-06-14 05:41:37 -0700

    Test: 5-number_keys.py

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    """a function that returns the number of keys
+       in a dictionary.
+
+       Args:
+        first arg: a_dictionary
+    """
+    key_list = list(a_dictionary)
+    return len(key_list)
