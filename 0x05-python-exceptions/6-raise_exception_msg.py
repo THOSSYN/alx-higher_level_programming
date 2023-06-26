9694eb5f840f71887b8a502b7f5c1d8a5c775fe6
THOSSYN <alextosin5@gmail.com>
2023-06-26 08:54:15 -0700

    Test: 6-raise_exception_msg.py

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    """a function that raises a name exception with a message.
+
+       Args:
+        first arg: message
+
+       Return: nothing
+    """
+    raise NameError(message)
