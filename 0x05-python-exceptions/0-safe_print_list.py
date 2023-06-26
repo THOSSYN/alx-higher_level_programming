4aa8fe80d9b86d994be7d12c28b0ebfe705051c8
THOSSYN <alextosin5@gmail.com>
2023-06-26 05:12:29 -0700

    Add: 0-safe_print_list.py README.md

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    """A function that prints x elements of a list
+       
+       Args:
+        first arg: my_list
+        second arg: x
+
+       Return: the real number of elements printed
+    """
+    try:
+        count = 0
+        for i in my_list:
+            if count < x:
+                print(i, end="")
+                count += 1
+            else:
+                break
+        print()
+    except:
+        pass
+    return count
