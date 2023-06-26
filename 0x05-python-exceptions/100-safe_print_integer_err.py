392f97609b01b3782fed166f7fbe02116a6ed993
THOSSYN <alextosin5@gmail.com>
2023-06-26 09:37:28 -0700

    Test: 100-safe_print_integer_err.py

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -13,6 +13,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except Exception:
+    except:
         sys.stderr.write("Exception: Unknown format code d' for object of type 'str'\n")
-    return False
+        return False
