504e9b3fd45793c1c381d8d4f511d94c07615d28
THOSSYN <alextosin5@gmail.com>
2023-06-27 09:42:01 -0700

    Test: Task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""Defines a class Square"""
+
+
 class Square:
+    """Represents a Square"""
+
     def __init__(self, size=0):
-        """Initializing size instance"""
+        """Initializing a new instance of Square
+
+           Args:
+            size (int): The size of the new square
+        """
         self.__size = size
