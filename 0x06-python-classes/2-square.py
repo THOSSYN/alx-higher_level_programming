c4fdbc2510a97e6921cd937345e8597cfdc20de4
THOSSYN <alextosin5@gmail.com>
2023-06-27 09:25:17 -0700

    Test: 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3
+# 2-square.py
+"""defines a class called square"""
+
+
 class Square:
-    """
-    This class represents a square.
-
-    Attribute:
-        __size (int): The size of the square.
-
-    Methods:
-        __init__(self, size=0): Initializes a Square object
-    """
+    """defines and initializes using __init__"""
     def __init__(self, size=0):
-        """
-        Initializes a Square object with a given size.
-
-        Args:
-            size (int): The size of the square
-
-        Raises:
-            TypeError: if the size is not an integer.
-            ValueError: If rhe size is less than 0
-        """
-        if not isinstance(size, int):
+        """handle errors"""
+        """check if size is not int"""
+        if type(size) is not int:
+            """prints out TypeError exception"""
             raise TypeError("size must be an integer")
+        """check if size is less than zero"""
         elif size < 0:
+            """
+            raise a ValueError exception because
+            minimum value should be zero
+            """
             raise ValueError("size must be >= 0")
+        """assigns value to an object"""
         self.__size = size
