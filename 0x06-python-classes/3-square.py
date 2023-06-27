5002ea43c6a8e069fd328c0a29a2a75cd9d91f24
THOSSYN <alextosin5@gmail.com>
2023-06-27 09:05:09 -0700

    Test: 3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+class Square:
+        """
+        Iniatializes a Square object
+
+        Attribute:
+            __size (int): The size of the square
+
+        Methods:
+            __init__(self, size=0): Initializes a Square object
+            area(self): returns the area of the square
+        """
+        def __init__(self, size=0):
+            """
+            Iniatializes a Square object
+
+            Args:
+                size (int): The size of the square
+
+            Raises:
+                TypeError: if size is not an integer
+                ValueError: if size is less than 0
+            """
+            if not isinstance(size, int):
+                raise TypeError("size must be an integer")
+            elif size < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = size
+        def area(self):
+            """
+            Calculate the area of a square.
+
+            Return:
+                int: The current square area.
+            """
+            return self.__size ** 2
