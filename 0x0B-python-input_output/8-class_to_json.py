6cdf793dd8c92a7480948f2e3fb0e547c69c59bb
THOSSYN <mtafortunefinder@gmail.com>
2023-07-11 17:42:55 -0700

    Updated: 8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -8,29 +8,16 @@ def class_to_json(obj):
 
        Args:
         obj: is the python object
-    
-    if isinstance(obj, MyClass):
-        return {'name': obj.name,
-                'number': obj.number}
-    else:
-        raise TypeError("Object is not serializable")
-    """
-    def class_to_json(obj):
-    """A function that returns the dictionary description with simple data structure
-    for JSON serialization of an object.
-
-    Args:
-        obj: The Python object to be serialized.
     """
     if not isinstance(obj, type):  # Check if obj is an instance of a Class
         raise TypeError("Object is not an instance of a Class")
 
     attributes = {}
 
-    for attr_name in dir(obj):  # Loop through all attributes of the object
-        if not attr_name.startswith("__"):  # Exclude dunder attributes
+    for attr_name in dir(obj):
+        if not attr_name.startswith("__"):
             attr_value = getattr(obj, attr_name)
-            if isinstance(attr_value, (list, dict, str, int, bool)):  # Check if attribute is serializable
+            if isinstance(attr_value, (list, dict, str, int, bool)):
                 attributes[attr_name] = attr_value
 
     return attributes
