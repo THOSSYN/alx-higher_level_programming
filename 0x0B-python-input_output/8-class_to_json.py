8dda447a21d8069109e7f2670b8a655905b368d0
THOSSYN <mtafortunefinder@gmail.com>
2023-07-11 17:33:24 -0700

    Updated: 8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -2,7 +2,7 @@
 """Class to json"""
 
 MyClass = __import__('8-my_class').MyClass
-
+MyClass = __import__('8-my_class_2').MyClass
 
 def class_to_json(obj):
     """A function that returns the dictionary description of
@@ -10,9 +10,25 @@ def class_to_json(obj):
 
        Args:
         obj: is the python object
-    """
+    
     if isinstance(obj, MyClass):
         return {'name': obj.name,
                 'number': obj.number}
+    else:
+        raise TypeError("Object is not serializable")
+    """
+    obj_class = type(obj)
+    if obj_class.__module__ == '__main__':
+        class_name = obj_class.__name__
+    else:
+        class_name = obj_class.__module__ + '.' + obj_class.__name__
+
+    if isinstance(obj, (list, dict, str, int, bool)):
+        return obj
+    elif isinstance(obj, MyClass) or isinstance(obj, MyClass2):
+        return {
+            '__class__': class_name,
+            **obj.__dict__
+        }
     else:
         raise TypeError("Object is not serializable")
