5b59d605ff06a65d50640c02181f289f27a532ea
THOSSYN <mtafortunefinder@gmail.com>
2023-07-12 07:03:36 -0700

    Updated: 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -10,4 +10,4 @@ def read_file(filename=""):
         Filename: is the name of the file
     """
     with open(filename, encoding='utf-8') as a_file:
-        print(a_file.read())
+        print(a_file.read(), end="")
