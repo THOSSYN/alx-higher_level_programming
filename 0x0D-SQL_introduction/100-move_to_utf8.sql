cd7001be91613caa2ac9623c8ff57f82b9aefece
THOSSYN <mtafortunefinder@gmail.com>
2023-08-15 15:42:16 -0700

    Added: 100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,5 @@
 -- Convert hbtn_0c_0 database, first_table table, and name field to UTF8 (utf8mb4)
 -- Field
-USE 'hbtn_0c_0'
+USE `hbtn_0c_0`
 ALTER TABLE first_table
 CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
