9514060bec20c27107309452d0718d089ae251c5
THOSSYN <mtafortunefinder@gmail.com>
2023-08-15 16:40:36 -0700

    Added: 103-max_state.sql

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,6 +1,6 @@
 -- Displays the max temperature of each state
 -- The order is by state name
-SELECT city, MAX(value) AS max_temp
+SELECT state, MAX(value) AS max_temp
 FROM temperatures
 GROUP BY state
 ORDER BY state ASC;
