a862aaaeea782a6bb2c41c4d67c90c858e197522
THOSSYN <mtafortunefinder@gmail.com>
2023-08-15 12:21:56 -0700

    Added: new files

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,7 +1,3 @@
 -- Show all tables in mysql server
 -- sets the db name to command line arg
-SET @db_name = 'mysql';
-
-SELECT table_name
-FROM information_schema.tables
-WHERE table_schema = @db_name;
+SHOW tables
