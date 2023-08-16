3fd66a0db92874dbf818b5c3cd4ad6ce130261f3
THOSSYN <mtafortunefinder@gmail.com>
2023-08-16 11:37:10 -0700

    Added: 6-state.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,8 +1,8 @@
 -- Creates the database hbtn_0d_usa and table state
 -- state is created inside hbtn_0d_usa
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`state` (
-	PRMARY KEY(id),
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
+	PRMARY KEY(`id`),
 	id INT NOT NULL AUTO_INCREMENT,
 	name VARCHAR(256) NOT NULL
 );
