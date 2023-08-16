bacb582ca4965db664911b79aba333ab108965df
THOSSYN <mtafortunefinder@gmail.com>
2023-08-16 13:59:05 -0700

    Added: 2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,11 +1,7 @@
 -- Creates database hbtn_0d_2
 -- Also creates user user_0d_2
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
-USE `hbtn_0d_2`
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
-   IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT
-   ON 'hbtn_0d_2'.*
-   TO 'user_0d_2'@'localhost'
-   IDENTIFIED BY 'user_0d_2_pwd';
+USE `hbtn_0d_2`;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
