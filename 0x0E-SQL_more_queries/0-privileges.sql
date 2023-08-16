8ed71872d83125536c14b8d33cf1cfcd45248069
THOSSYN <mtafortunefinder@gmail.com>
2023-08-16 13:20:19 -0700

    Added: 0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- Lists all privileges of a user on MySQL DB
 -- The user must already be created for this script to worrk
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
