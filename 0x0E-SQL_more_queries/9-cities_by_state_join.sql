60decf31bb156372e3a1041a80edf916d147627e
THOSSYN <mtafortunefinder@gmail.com>
2023-08-16 12:53:05 -0700

    Added: 9-cities_by_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,7 @@
 -- Lists all cities in hbtn_0d_usa
 -- Should have 3 columns
-SELECT c.`id`, c.`name`, s.`state`
-FROM cities AS c
-	INNER JOIN states AS s
+SELECT c.`id`, c.`name`, s.`name`
+FROM `cities` AS c
+	INNER JOIN `states` AS s
 	ON c.`state_id` = s.`id`
-ORDER BY c.`id` ASC;
+ORDER BY c.`id`;
