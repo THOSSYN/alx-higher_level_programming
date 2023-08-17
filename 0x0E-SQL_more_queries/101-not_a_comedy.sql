e062d56b413051d3c23c2374b26ef2a5f66920d8
THOSSYN <mtafortunefinder@gmail.com>
2023-08-16 18:56:21 -0700

    Added: 101-not_a_comedy.sql

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,11 @@
+-- Lists all shows without the genres comedy
+-- one record title
+SELECT DISTINCT tv_shows.title
+FROM tv_shows
+   LEFT JOIN tv_show_genres
+   ON tv_shows.id = tv_show_genres.show_id
+   LEFT JOIN tv_genres
+   ON tv_show_genres.genre_id = tv_genres.id
+   AND tv_genres.name = 'Comedy'
+   WHERE tv_genres.id IS NULL
+ORDER BY tv_shows.title;
