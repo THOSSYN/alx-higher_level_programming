52038aa63ad661507e627ea8a007e8464b433cdd
THOSSYN <mtafortunefinder@gmail.com>
2023-09-12 15:06:05 -0700

    Added: 6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+    this.size = size;
+  }
+  
+  charPrint(char = 'X') {
+    for (let i = 0; i < this.size; i++) {
+      let row = '';
+      for (let j = 0; j < this.size; j++) {
+        row += char;
+      }
+      console.log(row);
+    }
+  }
+}
+module.exports = Square;
