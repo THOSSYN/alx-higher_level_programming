115e78b45174d4df0f98fcb8855f3bbda1339ec9
THOSSYN <mtafortunefinder@gmail.com>
2023-09-15 04:46:24 -0700

    Added: 8-model_state_filter_first.py

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Lists all states objects in a database"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+if __name__ == '__main__':
+    my_uname = sys.argv[1]
+    my_pwd = sys.argv[2]
+    my_db = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(my_uname, my_pwd, my_db))
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    first_states = session.query(State).order_by(State.id).first()
+
+    for state in first_states:
+        print("{}: {}".format(state.id, state.name))
+        
+    session.close()
