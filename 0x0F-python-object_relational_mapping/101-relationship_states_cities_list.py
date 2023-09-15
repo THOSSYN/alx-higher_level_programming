67ff87265cc32bb140fbbcd4c4d6078c15813c32
THOSSYN <mtafortunefinder@gmail.com>
2023-09-15 12:01:39 -0700

    Added: 101-relationship_states_cities.py

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Creates a state with a city from a database"""
+
+import sys 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import Base, State
+from relationship_city import City
+
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state_list = session.query(State).order_by(State.id, City.id).all()
+
+    for state in state_list:
+        print("{}: {}".format(state.id, state.name))
+        for city in state.cities:
+            print("    {}: {}".format(city.id, city.name)
+
+    session.close()
