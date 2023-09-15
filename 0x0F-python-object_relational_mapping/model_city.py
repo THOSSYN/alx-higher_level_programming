8372ec11e06bd56c935667d0f6ae9d4c811fb94a
THOSSYN <mtafortunefinder@gmail.com>
2023-09-15 16:43:37 -0700

    corrected: model_city.py

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -5,11 +5,12 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from model_state import Base
 
+
 class City(Base):
     """A City class that inherits from State and Base"""
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True,
-            unique=True, nullable=False,
-            autoincrement=True)
+                unique=True, nullable=False,
+                autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
