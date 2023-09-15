f8bb33ef3a781c3f83190c246b7209f2f6281cc9
THOSSYN <mtafortunefinder@gmail.com>
2023-09-15 03:42:53 -0700

    Added: model_state.py

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a class for table creation"""
+
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+
+
+class State(Base):
+    __tablename__ = 'states'
+
+    id = Column(Integer, autoincrement=True, nullable=False,
+                unique=True, primary_key=True)
+    name = Column(String(128), nullable=False)
