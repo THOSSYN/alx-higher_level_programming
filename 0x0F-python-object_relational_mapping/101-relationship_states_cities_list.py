4bffc4f0dc3d9b8d22eabc768a276f4b8d6e73f0
THOSSYN <mtafortunefinder@gmail.com>
2023-09-15 13:38:57 -0700

    Added: 101-relationship_states_cities.py

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -7,7 +7,6 @@ from sqlalchemy.orm import sessionmaker
 from relationship_state import Base, State
 from relationship_city import City
 
-
 if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
@@ -17,11 +16,13 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state_list = session.query(State).order_by(State.id, City.id).all()
-
+    # Fetch State objects and their associated City objects, sorting both by ID
+    state_list = session.query(State).order_by(State.id).all()
+    
     for state in state_list:
         print("{}: {}".format(state.id, state.name))
-        for city in state.cities:
+        # Sort the associated City objects by ID
+        for city in sorted(state.cities, key=lambda c: c.id):
             print("  {}: {}".format(city.id, city.name))
 
     session.close()
