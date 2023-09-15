a95c4ab9eb4878134898e68dc2ea8175766bf79c
THOSSYN <mtafortunefinder@gmail.com>
2023-09-15 05:52:30 -0700

    Added: 10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-i
+
     my_search = session.query(State).filter_by(name=my_get).first()
 
     if my_search:
