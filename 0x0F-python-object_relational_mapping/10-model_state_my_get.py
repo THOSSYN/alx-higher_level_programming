0bda66512ee2c23f650dc9f1ab3b01250d2e5c6f
THOSSYN <mtafortunefinder@gmail.com>
2023-09-15 05:46:18 -0700

    Added: 10-model_state_my_get.py

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Lists all State objects containing 'a'"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+if __name__ == '__main__':
+    my_uname = sys.argv[1]
+    my_pwd = sys.argv[2]
+    my_db = sys.argv[3]
+    my_get = sys.argv[4]
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(my_uname, my_pwd, my_db))
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    my_search = session.query(State).filter_by(name=my_get).first()
+
+    if my_search:
+        print("{}".format(state.id))
+    else:
+        print("Not found")
+
+    session.close()
