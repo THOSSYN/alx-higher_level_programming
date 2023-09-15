a3940006bdb93e9414cdc39bff86775e9edafbdb
THOSSYN <mtafortunefinder@gmail.com>
2023-09-15 04:21:01 -0700

    Added: 7-model_state_filter_all.py

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Lists all states objects in a database"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == '__main__':
+    my_uname = sys.argv[1]
+    my_pwd = sys.argv[2]
+    my_db = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(my_uname, my_pwd, my_db))
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    all_states = session.query(States).order_by(states.id).all()
+
+    for state in all_states:
+        print("{}: {}".format(state.id, state.name))
