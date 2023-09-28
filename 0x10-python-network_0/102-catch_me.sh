675a6bb31950d54037ce59a3fc6782a6477c7012
THOSSYN <mtafortunefinder@gmail.com>
2023-09-28 12:57:20 -0700

    Added: 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # A script that cause the server to respond with "You got me"
-curl -s 0.0.0.0:5000/catch_me -o response.txt
-
-grep -o "You got me!" response.txt
-rm -f response.txt
+curl -s -L 0.0.0.0:5000/catch_me
