2277bd50b4e29e73fe925ed04f4be1faea1c34ea
THOSSYN <mtafortunefinder@gmail.com>
2023-09-28 13:50:33 -0700

    Added: 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,11 +2,11 @@
 
 def find_peak(list_of_integers):
     """Finds the peak of a list"""
+    if not list_of_integers:
+        return None
+
     length = len(list_of_integers) - 1
     mid = len(list_of_integers) // 2
-
-    if length < 0:
-        return None
 
     if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
         return list_of_integers[:mid]
