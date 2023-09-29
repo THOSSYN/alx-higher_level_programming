dd8a4919812276510ba01b1948f774ce800f5b19
THOSSYN <mtafortunefinder@gmail.com>
2023-09-29 13:13:49 -0700

    Added: 100-github_commits.py

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""A script that takes 2 arguments to find the
+   last 10 commits
+"""
+
+import sys
+import requests
+
+
+if __name__ == '__main__':
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    branch = "master"
+
+    headers = {
+            'accept': 'application/vnd.github.v3+json',
+            'Authorization': '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',
+            'X-GitHub-Api-Version': '2022-11-28',
+            }
+    url = f"https://api.github.com/repos/{owner}/{repo}/commits?sha={branch}"
+    response = requests.get(url, headers=headers)
+
+    commit = response.json()
+    print(commit)
