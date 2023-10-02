2a1a1ec9e483db11fbaa4e08e6f01bc8598da493
THOSSYN <mtafortunefinder@gmail.com>
2023-10-02 02:53:55 -0700

    Corrected 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,5 +8,5 @@ if __name__ == '__main__':
     response = requests.get('https://alx-intranet.hbtn.io/status')
     body = response.text
     print("Body response:")
-    print("    - type: {}".format(type(body)))
-    print("    - content: {}".format(body))
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
